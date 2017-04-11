6eda32a5581fc3d451d51d6d8f013f17be8ab982
finnd <dmfinn@wustl.edu>
2017-04-11 11:27:17 -0500

    interactive school picker added

--- Skeleton/FirstViewController.swift
+++ Skeleton/FirstViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class FirstViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
 
     
+    @IBOutlet weak var schoolTextField: UITextField!
     @IBOutlet weak var testLabel: UILabel!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var schoolPicker: UIPickerView!
     @IBOutlet weak var departmentPicker: UIPickerView!
-    var schoolPickerData = ["ArtSci","Engineering","Olin","Sam Fox","Brown"]
+    
+    var testPicker: UIPickerView! = UIPickerView()
+    
+    var schoolPickerData = ["All", "ArtSci","Engineering","Olin","Sam Fox","Brown"]
     
     var artSciData = ["Art class", "Science class"]
     var engineeringData = ["CompSci", "Something lame"]
@@ -37,6 +41,11 @@ class FirstViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
         schoolPicker.delegate = self
         departmentPicker.dataSource = self
         departmentPicker.delegate = self
+        self.schoolTextField.delegate = self
+        self.schoolTextField.inputView = testPicker
+        testPicker.isHidden = true
+        schoolPicker.isHidden = true
+        schoolTextField.text = schoolPickerData[0]
         
         let singleTap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         self.view.addGestureRecognizer(singleTap)
@@ -63,6 +72,8 @@ class FirstViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
             return schoolPickerData.count
         } else if pickerView == departmentPicker {
             return departmentPickerData.count
+        } else if pickerView == testPicker {
+            return schoolPickerData.count
         }
         return 0
     }
@@ -72,9 +83,13 @@ class FirstViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
             return schoolPickerData[row]
         } else if pickerView == departmentPicker {
             return departmentPickerData[row]
+        } else if pickerView == testPicker {
+            return schoolPickerData[row]
         }
         return ""
     }
+    
+    // depreciated
     @IBAction func updateLabel(_ sender: UIButton) {
         let searchText = self.searchBar.text
         self.testLabel.text = searchText
@@ -97,7 +112,7 @@ class FirstViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
     //stackoverflow.com/questions/31132807/how-can-i-populate-a-picker-view-depending-on-the-selection-of-another-picker-vi
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if pickerView == schoolPicker {
+        /*if pickerView == schoolPicker {
             switch row {
             case 0:
                 departmentPickerData = artSciData
@@ -111,11 +126,20 @@ class FirstViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
                 departmentPickerData = brownData
             default:
                 departmentPickerData = artSciData
+            }*/
+            if pickerView == schoolPicker {
+                self.schoolTextField.text = schoolPickerData[row]
+                self.schoolPicker.isHidden = true
             }
             // IMPORTANT reload the data on the item picker
         departmentPicker.reloadAllComponents()
-        }
         
+        
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        schoolPicker.isHidden = false
+        return false
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
