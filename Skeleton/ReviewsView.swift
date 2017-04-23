9ed8ec3af55eecad57bf04a6ce99da8992cbc82f
finnd <dmfinn@wustl.edu>
2017-04-23 16:41:46 -0500

    smart children commit and push;

--- Skeleton/ReviewsView.swift
+++ Skeleton/ReviewsView.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ReviewsView: UIViewController, UICollectionViewDataSource {
     
+    let DISPLAY_VIEW_TAG = 1
+    let OVERALL_LABEL_TAG = 2
+    let PROFESSOR_LABEL_TAG = 3
+    let COMMENTS_LABEL_TAG = 4
+    
     var reviews: [Review] = []
     var incomingCourseCode = ""
     
@@ -73,15 +78,34 @@ class ReviewsView: UIViewController, UICollectionViewDataSource {
         
         let review = reviews[indexPath.row]
         
-        let professorLabel = cell.viewWithTag(1) as! UILabel
+        let overallDisplayView = cell.viewWithTag(DISPLAY_VIEW_TAG) as! DisplayView
+        let overallLabel = cell.viewWithTag(OVERALL_LABEL_TAG) as! UILabel
+        let overallAsFloat = CGFloat(Double(review.overall) / 10.0)
+        
+        let professorLabel = cell.viewWithTag(PROFESSOR_LABEL_TAG) as! UILabel
+        professorLabel.text = "Professor: \(review.professors)"
+        
+        let commentsLabel = cell.viewWithTag(COMMENTS_LABEL_TAG) as! UILabel
+        
+        commentsLabel.text = review.comments
+        
+        overallLabel.text = String(review.overall)
+        overallDisplayView.color = getColorFromValue(value: review.overall)
+        overallDisplayView.value = overallAsFloat
+        
+        
+        
+        
+        
+        /*let professorLabel = cell.viewWithTag(1) as! UILabel
         let overallLabel = cell.viewWithTag(2) as! UILabel
         let gradingLabel = cell.viewWithTag(3) as! UILabel
-        let workloadLabel = cell.viewWithTag(4) as! UILabel
+        let workloadLabel = cell.viewWithTag(4) as! UILabel*/
         
-        professorLabel.text = review.professors
+        /*professorLabel.text = review.professors
         overallLabel.text = "Overall: \(String(review.overall))"
         gradingLabel.text = "Grading: \(String(review.grading))"
-        workloadLabel.text = "Workload: \(String(review.workload))"
+        workloadLabel.text = "Workload: \(String(review.workload))"*/
         
         cell.backgroundColor = UIColor(hue: 0.5611, saturation: 0.33, brightness: 0.96, alpha: 1.0)
         
@@ -89,6 +113,19 @@ class ReviewsView: UIViewController, UICollectionViewDataSource {
         cell.contentView.layer.borderWidth = 5
         
         return cell
+    }
+    
+    func getColorFromValue(value: Int) -> UIColor {
+        if (value < 4){
+            return UIColor.red
+        }
+        
+        if (value < 7){
+            return UIColor.yellow
+        }
+        
+        return UIColor.green
+    
     }
 
     override func didReceiveMemoryWarning() {
